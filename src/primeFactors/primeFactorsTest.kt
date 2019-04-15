a6d799730f8ec8acaef4211df549b0385bd0cb59
Toddvocado <todd.swift@gmail.com>
2019-04-15 19:52:55 -0400

    added a function that returned an empty list with a unit test to confirm

--- src/primeFactors/primeFactorsTest.kt
+++ src/primeFactors/primeFactorsTest.kt
@@ -1,10 +1,24 @@
 package primeFactors
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.*
 import org.junit.jupiter.api.Test
 
 class primeFactorsTest {
     /* Setup test harness using JUnit
     *  Running this will not do anything*/
-    @Test fun nothing() {}
+
+    /* todo: Write a tool that will calculate the prime factors of an integer */
+
+    @Test fun factors() {
+        // test that an empty list is returned upon execution
+        assertThat(factorsOf(1), `is` (emptyList()))
+    }
+
+    private fun factorsOf(i: Int): List<Int> {
+        return emptyList()
+    }
+
+
 
 }
