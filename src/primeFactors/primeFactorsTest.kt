56dfc4ea3cdc3c5371f84574a99cd00eebe42d81
Toddvocado <todd.swift@gmail.com>
2019-04-15 19:32:02 -0400

    added comments to explain test harness

--- src/primeFactors/primeFactorsTest.kt
+++ src/primeFactors/primeFactorsTest.kt
@@ -3,7 +3,8 @@ package primeFactors
 import org.junit.jupiter.api.Test
 
 class primeFactorsTest {
-
+    /* Setup test harness using JUnit
+    *  Running this will not do anything*/
     @Test fun nothing() {}
 
 }
